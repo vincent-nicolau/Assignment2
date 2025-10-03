6ee4b724e904b4033e706a97591a4476d4e15541
Vincent Nicolau <ven00002@mix.wvu.edu>
2025-10-03 12:59:39 -0400

    Add project files.

---
+++ Assignment2/MainForm.cs
@@ -0,0 +1,15 @@
+namespace Assignment2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
