e9159b11aeb93111f7072d8470a453a97c2b8a5b
vincent-nicolau <ven00002@mix.wvu.edu>
2026-02-18 15:52:01 -0500

    sync

--- Assignment2/Pages/Index.cshtml.cs
+++ Assignment2/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Assignment2.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +6,32 @@ namespace Assignment2.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        [BindProperty]
+        public PersonModel? PersonModel { get; set; }
         public void OnGet()
         {
+            _logger.LogInformation("Index - OnGet");
+            if (PersonModel != null)
+            {
+                _logger.LogInformation("Name={Name}", PersonModel.Name);
+                _logger.LogInformation("Age={Age}", PersonModel.Age);
+            }
+        }
 
+        public IActionResult OnPost()
+        {
+            _logger.LogInformation("Index - OnPost");
+            if (PersonModel != null)
+            {
+                _logger.LogInformation("Name={Name}", PersonModel.Name);
+                _logger.LogInformation("Age={Age}", PersonModel.Age);
+            }
+            return RedirectToPage("/PostRedirect", new {Name = PersonModel?.Name, Age = PersonModel?.Age});
         }
     }
 }
